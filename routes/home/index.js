9e6096ac1cee64e33febbd28ed5436768d4f08af
xiongsongsong <xiongsongsong@outlook.com>
2014-02-24 18:05:57 +0800

    更改为传统的递归方式

--- routes/home/index.js
+++ routes/home/index.js
@@ -5,5 +5,6 @@ var build = require('build')
 app.use(function *() {
     this.fileName = this.request.path
     this.dirName = __baseDirname
+    this.filePath = path.join(this.dirName, this.fileName)
     this.body = yield build.start
 });
