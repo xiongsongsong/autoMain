8e93e03d41c53cddfb2780dce3d86696fb42bf23
songsong <xiongsongsong@outlook.com>
2014-03-01 12:14:24 +0800

    增加读取静态文件的处理

--- routes/home/index.js
+++ routes/home/index.js
@@ -5,7 +5,7 @@ var build = require('build')
 app.use(function *() {
     this.fileName = this.request.path
     this.dirName = __baseDirname
-    this.rootUrl = 'https://alipay.com/'
+    this.rootUrl = 'http://localhost:8000/'
     this.rootProtocol = this.rootUrl.substring(0, this.rootUrl.indexOf('//') + 2)
     this.filePath = path.join(this.dirName, this.fileName)
     this.moduleCache = {}
