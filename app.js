b911b8e6e9f336c76a5c30f5c8c71c1ad9dcef91
xiongsongsong <xiongsongsong@outlook.com>
2014-03-05 14:26:04 +0800

    端口可自定义

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+var port = 8003
+
 var koa = require('koa');
 exports.app = koa();
 global.__baseDirname = __dirname
 
 require('./routes')
 
-exports.app.listen(8003);
+exports.app.listen(port);
 
 
 var exec = require('child_process').exec
 var OS = require('os')
 
-exec((OS.type().toLowerCase().indexOf('windows') > -1 ? 'start' : 'open') + ' http://localhost:8003')
+exec((OS.type().toLowerCase().indexOf('windows') > -1 ? 'start' : 'open') + ' http://localhost:' + port)
